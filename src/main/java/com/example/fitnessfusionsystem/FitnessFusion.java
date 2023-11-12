d8be2e90e896ee2d938519a0f971b446f9de326b
seguoo <seguo@farmingdale.edu>
2023-11-12 14:58:40 -0500

    completed user verification for age and weight, gender was switched to toggle radio buttons/ magenta color added for error in verification/ slight placement changes to UI (all in UserRegistration Screen)

--- src/main/java/com/example/fitnessfusionsystem/FitnessFusion.java
+++ src/main/java/com/example/fitnessfusionsystem/FitnessFusion.java
@@ -1,36 +1,36 @@
 package com.example.fitnessfusionsystem;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
-
-import java.io.FileInputStream;
 import java.io.IOException;
 
 public class FitnessFusion extends Application {
+    @FXML
+    public ImageView regScreen, logScreen;
 
     private static Scene scene;
-
+    /*
     @Override
     public void start(Stage stage) throws IOException {
-        FileInputStream serviceAccount =
-                new FileInputStream("path/to/serviceAccountKey.json");
-
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setDatabaseUrl("https://fitness-fusion-9ef62-default-rtdb.firebaseio.com")
-                .build();
-
-        FirebaseApp.initializeApp(options);
         scene = new Scene(loadFXML("explanation"));
         stage.setScene(scene);
         stage.show();
     }
+     */
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(FitnessFusion.class.getResource("Registration.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
--- src/main/java/com/example/fitnessfusionsystem/UserRegistrationController.java
+++ src/main/java/com/example/fitnessfusionsystem/UserRegistrationController.java
@@ -1,15 +1,19 @@
 package com.example.fitnessfusionsystem;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 
 public class UserRegistrationController {
     @FXML
-    private Label welcomeText, testFN, testLN, testEM, testPA, testNU;
+    private Label welcomeText, testFN, testLN, testEM, testPA, testNU, testA, testW;
     @FXML
-    private TextField fName, lName, email, passW1, newU;
+    private TextField fName, lName, email, passW1, newU, gender, age, weight;
+    @FXML
+    private RadioButton rButtonMale, rButtonFemale;
 
     boolean flag = false;
 
@@ -26,7 +30,7 @@ public class UserRegistrationController {
         });
         fName.focusedProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue) {
-                System.out.println("Welcome text is focused");
+                System.out.println("First name text is focused");
             } else {
                 if (fName.getText().matches("[a-zA-Z]{2,25}")) {
                     fName.setEditable(false);
@@ -54,7 +58,7 @@ public class UserRegistrationController {
         });
         lName.focusedProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue) {
-                System.out.println("Welcome text is focused");
+                System.out.println("Last name text is focused");
             } else {
                 if (lName.getText().matches("[a-zA-Z]{2,25}")) {
                     lName.setEditable(false);
@@ -82,7 +86,7 @@ public class UserRegistrationController {
         });
         email.focusedProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue) {
-                System.out.println("Welcome text is focused");
+                System.out.println("Email text is focused");
             } else {
                 if (email.getText().matches("^(.+)@(.+)$*")) {
                     email.setEditable(false);
@@ -110,9 +114,9 @@ public class UserRegistrationController {
         });
         passW1.focusedProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue) {
-                System.out.println("Welcome text is focused");
+                System.out.println("Password text is focused");
             } else {
-                if (passW1.getText().matches("[a-zA-Z0-9]{8}")) {
+                if (passW1.getText().matches("[a-zA-Z0-9]{8,25}")) {
                     passW1.setEditable(false);
                     passW1.setBorder(null);
                 } else {
@@ -138,7 +142,7 @@ public class UserRegistrationController {
         });
         newU.focusedProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue) {
-                System.out.println("Welcome text is focused");
+                System.out.println("New username text is focused");
             } else {
                 if (newU.getText().matches("[a-zA-Z0-9]{2,25}")) {
                     newU.setEditable(false);
@@ -154,11 +158,78 @@ public class UserRegistrationController {
 
             }
         });
+        age.setOnKeyPressed(key -> {
+
+            if (key.getCode() != KeyCode.TAB && flag) {
+                age.setStyle("-fx-border-color: lightgreen ; -fx-border-width: 4px ;");
+                testA.setText("");
+
+                flag = false;
+            }
+
+        });
+        age.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                System.out.println("Age text is focused");
+            } else {
+                if (age.getText().matches("[0-9]{2,3}")) {
+                    age.setEditable(false);
+                    age.setBorder(null);
+                } else {
+
+                    age.setStyle("-fx-border-color: red ; -fx-border-width: 4px ;");
+                    age.setVisible(true);
+                    age.requestFocus();
+                    testA.setText(age.getText() + " is an invalid age");
+                    flag = true;
+                }
+
+            }
+        });
+        weight.setOnKeyPressed(key -> {
+
+            if (key.getCode() != KeyCode.TAB && flag) {
+                weight.setStyle("-fx-border-color: lightgreen ; -fx-border-width: 4px ;");
+                testW.setText("");
+
+                flag = false;
+            }
+
+        });
+        weight.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                System.out.println("Weight text is focused");
+            } else {
+                if (weight.getText().matches("[0-9]{2,3}")) {
+                    weight.setEditable(false);
+                    weight.setBorder(null);
+                } else {
+
+                    weight.setStyle("-fx-border-color: red ; -fx-border-width: 4px ;");
+                    weight.setVisible(true);
+                    weight.requestFocus();
+                    testW.setText(weight.getText() + " is an invalid weight");
+                    flag = true;
+                }
+
+            }
+        });
 
 
         }
     @FXML
     protected void onHelloButtonClick () {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void selectGender (ActionEvent event) {
+
+        if(rButtonMale.isSelected()) {
+            gender.setText(rButtonMale.getText());
+        }
+
+        else if (rButtonFemale.isSelected()) {
+            gender.setText(rButtonFemale.getText());
+        }
     }
 }
